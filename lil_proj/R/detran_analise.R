b18c217ce6e5a6d8da06e16f60af5fc6f04be780
snoozleEmily <emilyalvimicm@gmail.com>
2025-07-16 20:58:43 -0300

    Update detran_analise.R

--- lil_proj/R/detran_analise.R
+++ lil_proj/R/detran_analise.R
@@ -5,7 +5,7 @@ setwd("d:/Projects/random projects/notes/notes/lil_proj/R")
 dataset <- read.csv(
   "datatran2024.csv",
   sep = ";",
-  fileEncoding = "Windows-1252"   # ou ""
+  fileEncoding = "Windows-1252" 
 )
 
 View(dataset)
