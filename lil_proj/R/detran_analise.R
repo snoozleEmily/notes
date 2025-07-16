b258e3423db616d723982ac8085bf86f9b5475e2
snoozleEmily <emilyalvimicm@gmail.com>
2025-07-16 20:56:19 -0300

    Create R Folder

---
+++ lil_proj/R/detran_analise.R
@@ -0,0 +1,11 @@
+# PROBABILIDADE E ESTATÍSTICA PARA ANÁLISE DE DADOS
+
+setwd("d:/Projects/random projects/notes/notes/lil_proj/R")
+
+dataset <- read.csv(
+  "datatran2024.csv",
+  sep = ";",
+  fileEncoding = "Windows-1252"   # ou ""
+)
+
+View(dataset)
