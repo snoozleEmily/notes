02387726b22137a4967ed589f066402b7abeb323
snoozleEmily <emilyalvimicm@gmail.com>
2025-07-18 17:24:43 -0300

    Update detran_analysis.R

--- lil_proj/R/detran/detran_analysis.R
+++ lil_proj/R/detran/detran_analysis.R
@@ -1,3 +1,4 @@
+library(skimr) 
 library(dplyr) # Ignore linter warnings
 if (getRversion() >= "2.15.1") {
   utils::globalVariables(c("%>%", "distinct", "across", "all_of"))
@@ -11,8 +12,13 @@ dataset <- read.csv(
   sep = ";",
   fileEncoding = "Windows-1252"
 )
-
 # View(dataset)
+
+# Overview
+# print(str(dataset))
+print(skim(dataset))
+
+# Distinct values
 source("D:/Projects/random projects/notes/notes/lil_proj/R/detran/distinct.R")
 distincts <- cols(dataset)
-View(distincts$cause)  # cause, type, condition
+# View(distincts$cause)  # cause, type, condition
