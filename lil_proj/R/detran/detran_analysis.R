6278b0c365a3e9500abbad512aef847b681e02f9
snoozleEmily <emilyalvimicm@gmail.com>
2025-07-21 17:08:41 -0300

    Add accident count bar plot by state
    Introduced ggplot2 to visualize accident counts by state using a bar chart.

--- lil_proj/R/detran/detran_analysis.R
+++ lil_proj/R/detran/detran_analysis.R
@@ -1,5 +1,6 @@
 library(skimr)
 library(dplyr)
+library(ggplot2)
 
 path <- "D:/Projects/random projects/notes/notes/lil_proj/R/detran"
 
@@ -21,13 +22,30 @@ source(file.path(path, "distinct.R"))
 source(file.path(path, "get_summarise.R"))
 
 # Ammount of accidents by state
-View(get_summarise(dataset, uf))
+by_state <- get_summarise(dataset, uf)
+# View(by_state) # 1. MG
+aes_var <- aes(x = reorder(uf, -acc), y = acc, fill = acc)
+plot_state <- ggplot(by_state, aes_var) +
+  geom_bar(stat = "identity") +
+  scale_fill_gradient(low = "paleturquoise", high = "midnightblue") +
+  labs(
+    title = "Quantidade de Acidentes por Estado (2024)",
+    x = "Estado (UF)",
+    y = "Número de Acidentes",
+    fill = "Número de Acidentes"
+  ) +
+  theme_minimal(base_size = 14)
+
+# print(plot_state)
 
 # Probability of climate influence
-View(get_summarise(dataset, condicao_meterologica))
+# View(get_summarise(dataset, condicao_metereologica)) # 1. Céu Claro
+
 
 # How the day time affects accidents
-View(get_summarise(dataset, fase_dia))
+# View(get_summarise(dataset, fase_dia)) # 1. Pleno dia
 
+# Most common types of accidents
+# View(get_summarise(dataset, causa_acidente))
 
 # What insights there are about types of prominent accidents and their causes
