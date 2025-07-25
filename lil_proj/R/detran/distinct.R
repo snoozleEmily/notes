a9086f378cac2d046c3dcfc370c17da1a6f3367a
snoozleEmily <emilyalvimicm@gmail.com>
2025-07-25 19:32:09 -0300

    Update distinct.R

--- lil_proj/R/detran/distinct.R
+++ lil_proj/R/detran/distinct.R
@@ -1,6 +1,12 @@
-distinct_col <- function(var, set, col) {
-  list(
-    var = set %>% distinct(col)
-  )
-  set %>% distinct(across(all_of(col)))
+library(dplyr)
+
+distinct_col <- function(set, col, n_name = "n", perc_name = "perc") {
+  set %>%
+    group_by(across(all_of(col))) %>% 
+    summarise(n = n(), .groups = "drop") %>%
+    mutate(perc = round((n / sum(n)) * 100, 2)) %>%
+    rename(
+      !!n_name := n,
+      !!perc_name := perc
+    )
 }
